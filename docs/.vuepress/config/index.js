7c77b2ec8d3baf61e31bd1227743210092ed5970
ganfengchi <ganfengchi521@163.com>
2023-08-14 12:17:15 +0800

    rollup 打包

--- docs/.vuepress/config/index.js
+++ docs/.vuepress/config/index.js
@@ -32,6 +32,10 @@ module.exports = {
       collapsible: true,
       children: [
         {
+          text: "rollup 构建项目",
+          link: "/view/sidebar/myExperience/rollup",
+        },
+        {
           text: "虚拟列表",
           link: "/view/sidebar/myExperience/virtualList",
         },
